6bd4ea204c999510b1c47243162d39be8085e776
VEERA PEKETI <satyanarayana.peketi28@gmail.com>
2026-05-31 15:29:18 -0400

    feat: add terraform/variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "task-api"
+}
+
+variable "environment" {
+  description = "Environment (dev / staging / prod)"
+  type        = string
+  default     = "dev"
+}
